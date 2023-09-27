3da758480b374b266e97227ee98bd368f4778f29
leticiapaoleschi <paoleschileticia480@gmail.com>
2023-09-27 20:33:56 -0300

    mandando meu primeiro hello word em php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>Olá mundo</title>
+  </head>
+  <body>
+    <?php echo '<p>Olá mundo, esse é meu primeiro programa em php</p>'; 
+
+      $x = 12;
+      $y = 10;
+      $soma = $x+$y;
+      $nome = "leticia";
+      echo  "meu nome é $nome, tenho $soma anos";
+    
+?> 
+  
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
